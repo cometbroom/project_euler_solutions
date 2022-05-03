dcff8fc0d80b32b251d4cc17a0bf420ac1ca498c
cometbroom <alawi.wow@gmail.com>
2022-05-03 17:13:24 +0200

    Made navigator dynamic

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,40 +1,62 @@
 import gsap from "gsap";
-import { INACTIVE_BTN, NAVIGATOR_QUERY, UI_QUERY } from "../constants";
+import {
+  INACTIVE_BTN,
+  MAIN_QUERY,
+  NAVIGATOR_QUERY,
+  UI_QUERY,
+} from "../constants";
 import { appData } from "../data/data";
 import router from "../lib/router";
 import { currentPage } from "../pages/problemPage";
+import { createElement } from "../tools/DOMCreate";
 import { addQuestionAnimation } from "./animations";
 
 //WN Write Jest testing for components and pages.
 
-let alreadyCreated = false;
+//Placeholders for our next and previous button elements
 let _prevBtn;
 let _nextBtn;
+
+//Keep track of current page in navigator to avoid timeout delay
 let _currentPagePure = null;
 
-export const setupNavigator = (state) => {
+export const setupNavigator = () => {
+  //
   if (_currentPagePure === null) _currentPagePure = currentPage;
+  //Populate next and previous button elements
   setSectionElements();
 
   setInactive([_prevBtn, _nextBtn]);
 
-  if (alreadyCreated) return;
   if (_currentPagePure > 0) setupButton(_prevBtn, "prev");
   if (_currentPagePure < appData.problems.length - 1)
     setupButton(_nextBtn, "next");
 };
 
 const setSectionElements = () => {
-  const section = document.querySelector(NAVIGATOR_QUERY);
+  let section = document.querySelector(NAVIGATOR_QUERY);
+  if (section === null) {
+    section = createNavigatorElements();
+  }
   [_prevBtn, _nextBtn] = section.querySelectorAll("button");
 };
 
-function setInactive(btns) {
+const createNavigatorElements = () => {
+  const main = document.querySelector(MAIN_QUERY);
+  const section = createElement("section", { class: "navigator" });
+  const btn1 = createElement("button", { content: "Previous" });
+  const btn2 = createElement("button", { content: "Next" });
+  section.append(btn1, btn2);
+  main.appendChild(section);
+  return section;
+};
+
+const setInactive = (btns) => {
   btns.forEach((btn) => {
     btn.classList.add(INACTIVE_BTN);
     btn.onclick = null;
   });
-}
+};
 
 const setupButton = (target, move) => {
   target.classList.remove(INACTIVE_BTN);
